3950a22d406763c660a2e91e20ac796c6aa268af
ThomasCiteU <totoipodbdc@gmail.com>
2022-01-07 14:19:59 +0100

    Add project files.

---
+++ DataSources/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSources
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+
+        public Album(int albumId, string title)
+        {
+            AlbumId = albumId;
+            Title = title;
+        }
+    }
+}
---
+++ LinqExercicePresentation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DataSources;
+using System.Linq;
+
+namespace LinqExercicePresentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Définir ma source de données
+            var malist = ListAlbumsData.ListAlbums;
+        }
+    }
+}
